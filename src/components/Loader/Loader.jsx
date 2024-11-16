163438fda8a74016fac03084f20a6c78dab7e574
Vladyslav <168086670+VladyslavPikuza@users.noreply.github.com>
2024-11-16 18:03:02 +0200

    fin

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,7 +1,8 @@
 import { Circles } from 'react-loader-spinner';
+import s from './Loader.module.css';
 
 const Loader = () => (
-  <div>
+  <div className={s.Loader}>
     <Circles color="#00BFFF" height={80} width={80} />
   </div>
 );
